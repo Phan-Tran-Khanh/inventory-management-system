5324f095a7e19374e462921b1f463c70f966bbe3
Phan-Tran-Khanh <ptrankhanh02@gmail.com>
2025-08-03 02:33:01 +0700

    test: Search products by category name

--- tests/Feature/Livewire/Tables/ProductTableTest.php
+++ tests/Feature/Livewire/Tables/ProductTableTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Feature\Livewire\Tables;
 
 use App\Livewire\Tables\ProductTable;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -15,5 +18,21 @@ class ProductTableTest extends TestCase
     {
         Livewire::test(ProductTable::class)
             ->assertStatus(200);
+    }
+
+    public function test_search_by_category_name()
+    {
+        Unit::factory()->count(3)->create();
+
+        $category1 = Category::factory()->create(['name' => 'Smartphone']);
+        $category2 = Category::factory()->create(['name' => 'Earphone']);
+
+        $product1 = Product::factory()->create(['name' => 'My Phone', 'category_id' => $category1->id]);
+        $product2 = Product::factory()->create(['name' => 'My Earphone', 'category_id' => $category2->id]);
+
+        Livewire::test('tables.product-table')
+            ->set('search', 'Smart')
+            ->assertSee($product1->name)
+            ->assertDontSee($product2->name);
     }
 }
